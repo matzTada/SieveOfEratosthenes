7460fbc72b0ce6e9605a17bc9ecbefcdb4a482f2
TadaMatz <matz@west.sd.keio.ac.jp>
2017-08-05 09:09:11 +0900

    change flap position by key input

--- sketch.js
+++ sketch.js
@@ -1,32 +1,32 @@
-var bugs = [];
+var MAX_NUM = 500;
+var rectSize = 10;
+var currentNum = 2;
 
 function setup() {
-  createCanvas(710, 400);
-  for (var i = 0; i < 50; i++) {
-    bugs.push(new Jitter());
-  }
+    createCanvas(800, 600);
 }
 
 function draw() {
-  background(50, 89, 100);
-  for (var i = 0; i < bugs.length; i++) {
-    bugs[i].move();
-    bugs[i].display();
-  }
+    background(50, 89, 100);
+    for (var i = 0; i < MAX_NUM; i++) {
+      rectSize = width / 30;
+        var x = floor(i % currentNum) * rectSize;
+        var y = floor(i / currentNum) * rectSize;
+        fill(255);
+        rect(x, y, rectSize, rectSize);
+        fill(0);
+        textSize(rectSize * 0.2);
+        text(i, x, y + rectSize);
+    }
 }
 
-function Jitter() {
-  this.x = random(width);
-  this.y = random(height);
-  this.diameter = random(10, 30);
-  this.speed = 5;
-
-  this.move = function() {
-    this.x += random(-this.speed, this.speed);
-    this.y += random(-this.speed, this.speed);
-  };
-
-  this.display = function() {
-    ellipse(this.x, this.y, this.diameter, this.diameter);
-  }
-};
+function keyTyped() {
+    switch (key) {
+        case 'a':
+            currentNum += 1;
+            break;
+       case 'A':
+            currentNum -= 1;
+            break;    }
+    return false;
+}
